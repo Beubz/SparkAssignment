dc7bc0dbbfdbafea89e793a173a3af9fe23fad64
beubz <arthur.beurel@gmail.com>
2015-12-01 15:27:05 +0100

    Few fonctions added

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "Assignment"
+version := "1.0"
+scalaVersion := "2.10.4"
+mainClass := Some("main")
+
+libraryDependencies += "org.apache.commons" % "commons-csv" % "1.2"
+libraryDependencies += "net.sf.opencsv" % "opencsv" % "2.3"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.2"
+libraryDependencies += "org.scala-lang" % "scala-compiler" % scalaVersion.value
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.5.2"
---
+++ main.scala
@@ -0,0 +1,83 @@
+import au.com.bytecode.opencsv.CSVParser
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+
+
+
+
+
+object Assignment {
+
+	case class Crime(date:String , adress:String , district:Int , beat:String , grid:Int, crimedescr:String , ucrCode:Int, latitude:String , longitude:String )
+
+	def main(args: Array[String]){
+
+		val conf = new SparkConf().setAppName("Assignment").setMaster("local")
+		val sc = new SparkContext(conf)
+
+		val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+		// this is used to implicitly convert an RDD to a DataFrame.
+		import sqlContext.implicits._
+
+		val file = sc.textFile("crimes.csv").mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
+
+		val crimes = file.map(line => {
+			val l=line.split(",")
+			Crime(l(0),l(1),l(2).toInt,l(3),l(4).toInt,l(5),l(6).toInt, l(7), l(8))
+			})
+
+		val crimesDF = crimes.toDF()
+
+		var input="nul"
+
+		while (input != 0)
+		{
+			println("What do you want to know ?")
+			println("1. The crime that happens the most in Sacramento ? (RDD Style)")
+			println("2. The 3 days with the highest crime count ?")
+			println("3. The average of each crime per day ?")
+			println("4. What is the crime that happens the most in Sacramento ? (DF style")
+			println("5. The 3 days with the highest crime count ?")
+			println("6. The average of each crime per day ?")
+			input = readLine("Choose wisely > ")
+
+				input match{
+					case "1" => 
+					{
+						file.mapPartitions(lines => {
+							val parser=new CSVParser(',')
+							lines.map(line => {
+								val columns = parser.parseLine(line)
+								Array(columns(5)).mkString(",")
+								})
+							}).countByValue().toList.sortBy(-_._2).take(1).foreach(println)
+					}
+
+					case "2" =>
+					{
+						file.mapPartitions(lines => {
+							val parser=new CSVParser(',')
+							lines.map(line => {
+								val columns = parser.parseLine(line)
+								Array(columns(0).split(" ")(0)).mkString(",")
+								})
+							}).countByValue().toList.sortBy(-_._2).take(3).foreach(println)
+					}
+
+					case "4" =>
+					{
+						crimesDF.groupBy('crimedescr).count().sort('count.desc).show(1)
+					}
+
+					case "5" =>
+					{
+						crimesDF.groupBy('date.substr(0, 7)).count().sort('count.desc).show(3)
+					}
+				}		
+			}
+		}
+
+}
