9f3072aae73db84313514e1c339787099156ac14
beubz <arthur.beurel@gmail.com>
2015-12-02 18:31:18 +0100

    CSV export

--- build.sbt
+++ build.sbt
@@ -8,3 +8,4 @@ libraryDependencies += "net.sf.opencsv" % "opencsv" % "2.3"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.2"
 libraryDependencies += "org.scala-lang" % "scala-compiler" % scalaVersion.value
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.5.2"
+libraryDependencies += "com.databricks" % "spark-csv_2.10" % "0.1"
--- main.scala
+++ main.scala
@@ -3,6 +3,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
+import com.databricks.spark.csv
 
 
 object Assignment {
@@ -32,7 +33,7 @@ object Assignment {
 
 		while (input != 0)
 		{
-			println("What do you want to know ? \n")
+			println("\nWhat do you want to know ? \n")
 			println("1. The crime that happens the most in Sacramento ? (RDD Style)")
 			println("2. The 3 days with the highest crime count ?")
 			println("3. The average of each crime per day ?")
@@ -40,58 +41,67 @@ object Assignment {
 			println("5. The 3 days with the highest crime count ?")
 			println("6. The average of each crime per day ? \n")
 			println("0. QUIT \n")
-			
+
 			input = readLine("Choose wisely > ")
 
-				input match{
-					case "1" => 
-					{
-						file.mapPartitions(lines => {
-							val parser=new CSVParser(',')
-							lines.map(line => {
-								val columns = parser.parseLine(line)
-								Array(columns(5)).mkString(",")
-								})
-							}).countByValue().toList.sortBy(-_._2).take(1).foreach(println)
-					}
+			input match{
+				case "1" => 
+				{
+					file.mapPartitions(lines => {
+						val parser=new CSVParser(',')
+						lines.map(line => {
+							val columns = parser.parseLine(line)
+							Array(columns(5)).mkString(",")
+							})
+						}).countByValue().toList.sortBy(-_._2).take(1).foreach(println)
+				}
 
-					case "2" =>
-					{
-						file.mapPartitions(lines => {
-							val parser=new CSVParser(',')
-							lines.map(line => {
-								val columns = parser.parseLine(line)
-								Array(columns(0).split(" ")(0)).mkString(",")
-								})
-							}).countByValue().toList.sortBy(-_._2).take(3).foreach(println)
-					}
-					case "3" =>
-					{
+				case "2" =>
+				{
+					file.mapPartitions(lines => {
+						val parser=new CSVParser(',')
+						lines.map(line => {
+							val columns = parser.parseLine(line)
+							Array(columns(0).split(" ")(0)).mkString(",")
+							})
+						}).countByValue().toList.sortBy(-_._2).take(3).foreach(println)
+				}
+				case "3" =>
+				{
 
-					}
+				}
 
-					case "4" =>
-					{
-						val result = crimesDF.groupBy('crimedescr).count().sort('count.desc)
-						result.withColumnRenamed("crimedescr", "Crime").show(1)
-					}
+				case "4" =>
+				{
+					val result = crimesDF.groupBy('crimedescr).count().sort('count.desc)
+					result.withColumnRenamed("crimedescr", "Crime").show(1)
+				}
 
-					case "5" =>
-					{
-						val result = crimesDF.groupBy('date.substr(0, 7)).count().sort('count.desc)
-						result.withColumnRenamed("substring(date,0,7)", "Date").withColumnRenamed("count", "Crime Count").show(3)
-					}
+				case "5" =>
+				{
+					val result = crimesDF.groupBy('date.substr(0, 7)).count().sort('count.desc)
+					result.withColumnRenamed("substring(date,0,7)", "Date").withColumnRenamed("count", "Crime Count").show(3)
+				}
 
-					case "6" =>
-					{
-						val crimesCount = crimesDF.groupBy('crimedescr).count()
-						val day = crimesDF.groupBy('date.substr(0, 7)).count()
-						val result = crimesCount.select(crimesCount("crimedescr"), crimesCount("count")/day.count)
-						result.withColumnRenamed("crimedescr", "Crime").withColumnRenamed("(count / 31)", "Per Day").show()
+				case "6" =>
+				{
+					val crimesCount = crimesDF.groupBy('crimedescr).count()
+					val days = crimesDF.groupBy('date.substr(0, 7)).count()
+					val result = crimesCount.select(crimesCount("crimedescr"), crimesCount("count")/days.count)
+					result.withColumnRenamed("crimedescr", "Crime").withColumnRenamed("(count / 31)", "Per Day").show()
 
-					}
-				}		
-			}
+				}
+
+				case "7" =>
+				{
+					val crimesCount = crimesDF.groupBy('crimedescr).count()
+					val days = crimesDF.groupBy('date.substr(0, 7)).count()
+					val result = crimesCount.select(crimesCount("crimedescr"), crimesCount("count")/days.count)
+					val csv = result.withColumnRenamed("crimedescr", "Crime").withColumnRenamed("(count / 31)", "Per Day")
+					csv.rdd.map(x=>x.mkString(",")).coalesce(1).saveAsTextFile("csvFile")
+
+				}
+			}		
 		}
-
+	}
 }
